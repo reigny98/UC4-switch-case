278f778942054934d838b90abf5295b53922335d
reigny98 <reigny2016@gmail.com>
2022-02-28 00:03:09 -1200

    added UC4 program

---
+++ UC4-switch case/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace UC4_switch_case
+{
+    internal class Program
+    {
+        public static void Main(String[] args)
+        {
+
+            int EMP_RATE_PER_HOUR = 20;
+            //Variables
+            int empHrs = 0;
+            int empWages = 0;
+            Random random = new Random();
+            int hours = random.Next(0, 3);
+            switch (hours)
+            {
+
+                case 1:
+                    empHrs = 8;
+                    break;
+
+                case 2:
+                    empHrs = 4;
+                    break;
+
+                default :
+                    empHrs = 0;
+                    break;
+         
+            }
+            empWages = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wages :" + empWages);
+
+        }
+
+    }
+}
